8d6982db2a283c4ebe955ea61e42ca6bc5647c74
Bruno Baccho Terra <brunobacchot@hotmail.com>
2021-01-02 14:09:23 -0300

    Adicionado Shapes/Objects nas texturas.

--- core/src/com/bacchoterra/mariobros/screens/PlayScreen.java
+++ core/src/com/bacchoterra/mariobros/screens/PlayScreen.java
@@ -6,11 +6,21 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTile;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 
@@ -31,23 +41,73 @@ public class PlayScreen implements Screen {
     private TiledMap map;
     private OrthogonalTiledMapRenderer mapRenderer;
 
-    //TouchPosition
-    private Vector3 vector3;
+    //Box2D
+    private World world;
+    private Box2DDebugRenderer box2DDebugRenderer;
+
+    BodyDef bodyDef;
+    FixtureDef fixtureDef;
+    PolygonShape polygonShape;
+    Body body;
+
 
     public PlayScreen(Jogo jogo) {
         this.jogo = jogo;
 
         gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(Jogo.V_WIDTH/Jogo.PPM,Jogo.V_HEIGHT/Jogo.PPM,gameCam);
+        gamePort = new FitViewport(Jogo.V_WIDTH / Jogo.PPM, Jogo.V_HEIGHT / Jogo.PPM, gameCam);
 
         hud = new Hud(jogo.batch);
 
         mapLoader = new TmxMapLoader();
         map = mapLoader.load("level1.tmx");
-        mapRenderer = new OrthogonalTiledMapRenderer(map,1/Jogo.PPM);
-        gameCam.position.set(gamePort.getWorldWidth()/2,gamePort.getWorldHeight()/2f,0);
+        mapRenderer = new OrthogonalTiledMapRenderer(map, 1 / Jogo.PPM);
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2f, 0);
 
-        vector3 = new Vector3();
+        world = new World(new Vector2(0, 0), true);
+        box2DDebugRenderer = new Box2DDebugRenderer();
+
+        bodyDef = new BodyDef();
+        fixtureDef = new FixtureDef();
+        polygonShape = new PolygonShape();
+
+        criarObjetos();
+
+
+    }
+
+    private void criarObjetos() {
+
+        for (MapObject object : map.getLayers().get(2).getObjects().getByType(RectangleMapObject.class)) {
+
+            Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+            bodyDef.position.set((rectangle.getX() + rectangle.getWidth()/2) /Jogo.PPM,(rectangle.getY() + rectangle.getHeight()/2) /Jogo.PPM);
+            body = world.createBody(bodyDef);
+            polygonShape.setAsBox(rectangle.getWidth()/2 / Jogo.PPM,rectangle.getHeight()/2/ Jogo.PPM);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+
+
+        }
+
+        for (MapObject object : map.getLayers().get(3).getObjects().getByType(RectangleMapObject.class)) {
+
+            Rectangle rectangle = ((RectangleMapObject) object).getRectangle();
+            bodyDef.type = BodyDef.BodyType.StaticBody;
+
+
+            bodyDef.position.set((rectangle.getX() + rectangle.getWidth()/2) /Jogo.PPM,(rectangle.getY() + rectangle.getHeight()/2) /Jogo.PPM);
+
+            body = world.createBody(bodyDef);
+
+            polygonShape.setAsBox(rectangle.getWidth()/2 / Jogo.PPM,rectangle.getHeight()/2/ Jogo.PPM);
+            fixtureDef.shape = polygonShape;
+            body.createFixture(fixtureDef);
+
+
+        }
+
 
     }
 
@@ -63,6 +123,7 @@ public class PlayScreen implements Screen {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         mapRenderer.render();
+        box2DDebugRenderer.render(world,gameCam.combined);
 
 
         jogo.batch.setProjectionMatrix(hud.stage.getCamera().combined);
@@ -73,7 +134,7 @@ public class PlayScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        gamePort.update(width,height);
+        gamePort.update(width, height);
     }
 
     @Override
@@ -96,7 +157,7 @@ public class PlayScreen implements Screen {
 
     }
 
-    private void update(float dt){
+    private void update(float dt) {
         handleInput(dt);
         gameCam.update();
         mapRenderer.setView(gameCam);
@@ -106,24 +167,28 @@ public class PlayScreen implements Screen {
 
     private void handleInput(float dt) {
 
-        if (Gdx.input.justTouched()){
+        /*if (Gdx.input.isTouched()){
 
             vector3.set(Gdx.input.getX(), Gdx.input.getY(),0);
-            gamePort.unproject(vector3);
 
-            Gdx.app.log("sexo",String.valueOf((int)vector3.x));
+            Gdx.app.log("TouchCoord","X pos -> " + vector3.x + " // " + "Y pos -> " + vector3.y);
+
 
         }
 
-       /* if (Gdx.input.isTouched() && vector3.x >1){
-            gameCam.position.x += 10 *dt;
+         */
+
+        if (Gdx.input.isTouched() && Gdx.input.getX() > Gdx.graphics.getWidth() / 2) {
+            gameCam.position.x += 7 * dt;
+            Gdx.app.log("TouchCoord", "X pos -> " + Gdx.input.getX() + " // " + "Y pos -> " + Gdx.input.getY());
         }
 
-        if (Gdx.input.isTouched() && vector3.x <=1){
-            gameCam.position.x -= 10 *dt;
+        if (Gdx.input.isTouched() && Gdx.input.getX() < Gdx.graphics.getWidth() / 2) {
+            gameCam.position.x -= 7 * dt;
+            Gdx.app.log("TouchCoord", "X pos -> " + Gdx.input.getX() + " // " + "Y pos -> " + Gdx.input.getY());
+
         }
 
-        */
 
     }
 }
