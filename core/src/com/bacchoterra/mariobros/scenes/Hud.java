b5723719ee4576aaf76d922c6398727a8720ec66
Bruno Baccho Terra <brunobacchot@hotmail.com>
2021-01-02 00:13:15 -0300

    Hud adicionado.

--- core/src/com/bacchoterra/mariobros/scenes/Hud.java
+++ core/src/com/bacchoterra/mariobros/scenes/Hud.java
@@ -1,8 +1,53 @@
 package com.bacchoterra.mariobros.scenes;
 
+import com.bacchoterra.mariobros.Jogo;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+
 
 public class Hud {
 
+    public Stage stage;
+    public Viewport viewport;
+
+    private Label timeLabel,worldLabel,scoreLabel;
+
+
+    public Hud(SpriteBatch sb) {
+
+        viewport = new FitViewport(Jogo.V_WIDTH,Jogo.V_HEIGHT,new OrthographicCamera());
+
+        stage = new Stage(viewport,sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        initLabels();
+
+        table.add(timeLabel).expandX().padTop(10);
+        table.add(worldLabel).expandX().padTop(10);
+        table.add(scoreLabel).expandX().padTop(10);
+
+        stage.addActor(table);
+
+
+
+    }
+
+    private void initLabels(){
+
+        timeLabel = new Label("300", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        worldLabel = new Label("1-1", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label("45", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+    }
 }
--- core/src/com/bacchoterra/mariobros/screens/PlayScreen.java
+++ core/src/com/bacchoterra/mariobros/screens/PlayScreen.java
@@ -1,7 +1,10 @@
 package com.bacchoterra.mariobros.screens;
 
 import com.bacchoterra.mariobros.Jogo;
+import com.bacchoterra.mariobros.scenes.Hud;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -15,11 +18,17 @@ public class PlayScreen implements Screen {
     private OrthographicCamera gameCam;
     private Viewport gamePort;
 
+    //Hud
+    private Hud hud;
+
     public PlayScreen(Jogo jogo) {
         this.jogo = jogo;
 
         gameCam = new OrthographicCamera();
         gamePort = new FitViewport(Jogo.V_WIDTH/Jogo.PPM,Jogo.V_HEIGHT/Jogo.PPM);
+
+        hud = new Hud(jogo.batch);
+
     }
 
     @Override
@@ -29,8 +38,12 @@ public class PlayScreen implements Screen {
 
     @Override
     public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        jogo.batch.setProjectionMatrix(gameCam.combined);
+
+        jogo.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
 
 
     }
